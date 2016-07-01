889f980fea8490f4a8c48ea6a2df277090a38baf
LChukka <lchukkamail@gmail.com>
2016-07-01 18:22:45 +0800

    Add files via upload

---
+++ webservices/src/main/java/com/ambali/webservices/TestMain.java
@@ -0,0 +1,13 @@
+package com.ambali.webservices;
+
+/**
+ * Created by laxminarsimhar on 01/07/2016.
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+        System.out.print("testing app");
+    }
+
+
+}
